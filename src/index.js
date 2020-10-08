54ffd7dc9a596132795c009515574eac258bdc63
Alberto <albertocruzadopleite@gmail.com>
2020-10-08 04:54:50 +0200

    Tomando snapshot de un ejercicio terminado

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+//Bob Ziroll is providing some of his personal choices in regards to:
+//1. not using semicolon at the end of statements 
+//2. using arrow functions, instead of the standalone funtions we have used so far, on certain cases
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//Example of an arrow function:
+/*
+const App = () => <h1>Hello world!</h1>
+*/
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+function App() {
+    return (
+       <h1>Hello world!</h1>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
